3f7b951148ed7d1f92529bd4d469a7fa81d4f6c0
Arlind <arlind12344@gmail.com>
2024-04-18 15:45:07 +0200

    Improvements

--- color_changer/content_script.js
+++ color_changer/content_script.js
@@ -7,6 +7,8 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 document.addEventListener('DOMContentLoaded', () => {
 	chrome.storage.sync.get(
 		{
+			enabledTab: 'Color',
+			name: '',
 			color: '#f4a3f1',
 			newColor: '#ff0000',
 			waitForLoad: 0,
@@ -15,15 +17,38 @@ document.addEventListener('DOMContentLoaded', () => {
 			applyOnLoad: false,
 		},
 		(items) => {
-			if (items.applyOnLoad) {
-				console.log(`Waiting for ${items.waitForLoad} seconds to apply changes...`);
-				setTimeout(() => {
-					changeColorOrImage(hexToRGB(items.color), hexToRGB(items.newColor), items.imageUrl, items.useImageInstead);
-				}, items.waitForLoad * 1000);
+			if (typeof window['handle' + items.enabledTab] === 'function') {
+				window['handle' + items.enabledTab](items);
 			}
+
+			setInterval(() => {
+				items.waitForLoad = 0;
+				if (typeof window['handle' + items.enabledTab] === 'function') {
+					window['handle' + items.enabledTab](items);
+				}
+			}, 15000);
 		}
+
 	);
 });
+
+function handleColor(items) {
+	if (items.applyOnLoad) {
+		console.log(`Waiting for ${items.waitForLoad} seconds to apply changes...`);
+		setTimeout(() => {
+			changeColorOrImage(hexToRGB(items.color), hexToRGB(items.newColor), items.imageUrl, items.useImageInstead);
+		}, items.waitForLoad * 1000);
+	}
+}
+
+function handleName(items) {
+	if (items.applyOnLoad) {
+		console.log(`Waiting for ${items.waitForLoad} seconds to apply changes...`);
+		setTimeout(() => {
+			changeColorOrImageWithName(items.name, hexToRGB(items.newColor), items.imageUrl, items.useImageInstead);
+		}, items.waitForLoad * 1000);
+	}
+}
 
 function hexToRGB(hex) {
 	const r = parseInt(hex.substring(1, 3), 16);
@@ -44,15 +69,45 @@ function findElementsWithColor(color) {
 	return foundElements;
 }
 
+function findElementsWithName(name) {
+	const elements = document.querySelectorAll('*');
+	const foundElements = [];
+	elements.forEach(element => {
+		if (element.innerText === name) {
+			foundElements.push(element);
+		}
+	});
+	return foundElements;
+}
+
 function changeColorOrImage(colorToReplace, colorToReplaceWith, imageUrl = '', useImageInstead = false) {
 	let foundElements = findElementsWithColor(colorToReplace);
 	foundElements.forEach(element => {
-		if (imageUrl !== '' && useImageInstead) {
-			element.style.setProperty('background-image', `url(${imageUrl})`, 'important');
-			element.style.setProperty('background-size', 'cover', 'important');
-		} else {
-			element.style.setProperty('background-color', colorToReplaceWith, 'important');
+		let currentColor = window.getComputedStyle(element).getPropertyValue('background-color');
+		if (currentColor !== colorToReplaceWith) {
+			if (imageUrl !== '' && useImageInstead) {
+				element.style.setProperty('background-image', `url(${imageUrl})`, 'important');
+				element.style.setProperty('background-size', 'cover', 'important');
+			} else {
+				element.style.setProperty('background-color', colorToReplaceWith, 'important');
+			}
+			console.log(`Changed color from ${colorToReplace} to ${colorToReplaceWith}`);
 		}
 	});
-	console.log(`Changed color from ${colorToReplace} to ${colorToReplaceWith}`);
+}
+
+function changeColorOrImageWithName(name, colorToReplaceWith, imageUrl = '', useImageInstead = false) {
+	let foundElements = findElementsWithName(name);
+	foundElements.forEach(element => {
+		let currentColor = window.getComputedStyle(element).getPropertyValue('background-color');
+		if (currentColor !== colorToReplaceWith) {
+			if (imageUrl !== '' && useImageInstead) {
+				element.style.setProperty('background-image', `url(${imageUrl})`, 'important');
+				element.style.setProperty('background-size', 'cover', 'important');
+			} else {
+				element.style.setProperty('background-color', colorToReplaceWith, 'important');
+			}
+			console.log(`Changed color of elements with name ${name} to ${colorToReplaceWith}`);
+		}
+	});
 }
--- color_changer/options.js
+++ color_changer/options.js
@@ -1,76 +1,128 @@
 // Saves options to chrome.storage
 const saveOptions = () => {
-    const color = document.getElementById('color').value;
-    const newColor = document.getElementById('newColor').value;
-    const applyOnLoad = document.getElementById('apply-on-load').checked;
-    const waitForLoad = document.getElementById('wait-for-load').value;
-    const useImageInstead = document.getElementById('use-image-instead').checked;
-    const imageUrl = document.getElementById('imageUrl').value;
+	const enabledTab = document.querySelector('.tablinks.active').textContent;
+	const name = document.getElementById('name-option').value;
+	const color = document.getElementById('color').value;
+	const newColor = document.getElementById('newColor').value;
+	const applyOnLoad = document.getElementById('apply-on-load').checked;
+	const waitForLoad = document.getElementById('wait-for-load').value;
+	const useImageInstead = document.getElementById('use-image-instead').checked;
+	const imageUrl = document.getElementById('imageUrl').value;
 
-    chrome.storage.sync.set(
-        { color: color, newColor: newColor, useImageInstead: useImageInstead, imageUrl: imageUrl, applyOnLoad: applyOnLoad, waitForLoad: waitForLoad },
-        () => {
-            const status = document.getElementById('status');
-            status.textContent = 'Options saved.';
-            setTimeout(() => {
-                status.textContent = '';
-            }, 750);
-        }
-    );
+	chrome.storage.sync.set(
+		{
+			enabledTab: enabledTab,
+			name: name,
+			color: color,
+			newColor: newColor,
+			useImageInstead: useImageInstead,
+			imageUrl: imageUrl,
+			applyOnLoad: applyOnLoad,
+			waitForLoad: waitForLoad
+		},
+		() => {
+			const status = document.getElementById('status');
+			status.textContent = 'Options saved.';
+			setTimeout(() => {
+				status.textContent = '';
+			}, 750);
+		}
+	);
 };
 
 const restoreOptions = () => {
-    chrome.storage.sync.get(
-        {
-            color: '#f4a3f1',
-            newColor: '#ff0000',
-            applyOnLoad: false,
-            waitForLoad: 0,
-            useImageInstead: false,
-            imageUrl: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
-        },
-        (items) => {
-            document.getElementById('color').value = items.color;
-            document.getElementById('newColor').value = items.newColor;
-            document.getElementById('apply-on-load').checked = items.applyOnLoad;
-            document.getElementById('wait-for-load').value = items.waitForLoad;
-            document.getElementById('use-image-instead').checked = items.useImageInstead;
-            document.getElementById('imageUrl').value = items.imageUrl;
+	chrome.storage.sync.get(
+		{
+			enabledTab: 'Color',
+			name: '',
+			color: '#f4a3f1',
+			newColor: '#ff0000',
+			applyOnLoad: false,
+			waitForLoad: 0,
+			useImageInstead: false,
+			imageUrl: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
+		},
+		(items) => {
+			document.getElementById(`${items.enabledTab.toLowerCase()}Tab`).click();
 
-            if (items.useImageInstead) {
-                document.getElementById('newColor').disabled = true;
-            }
-        }
-    );
+			document.getElementById('name-option').value = items.name;
+			document.getElementById('color').value = items.color;
+			document.getElementById('newColor').value = items.newColor;
+			document.getElementById('apply-on-load').checked = items.applyOnLoad;
+			document.getElementById('wait-for-load').value = items.waitForLoad;
+			document.getElementById('use-image-instead').checked = items.useImageInstead;
+			document.getElementById('imageUrl').value = items.imageUrl;
+
+			if (items.useImageInstead) {
+				document.getElementById('newColor').disabled = true;
+			}
+		}
+	);
 };
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
 document.getElementById('save').addEventListener('click', saveOptions);
 
 document.getElementById('use-image-instead').addEventListener('change', function () {
-    let imageUrlInput = document.getElementById('image-url');
-    let newColor = document.getElementById('newColor');
-    if (this.checked) {
-        imageUrlInput.style.maxHeight = '100px';
-        imageUrlInput.style.opacity = '1';
+	let imageUrlInput = document.getElementById('image-url');
+	let newColor = document.getElementById('newColor');
+	if (this.checked) {
+		imageUrlInput.style.maxHeight = '100px';
+		imageUrlInput.style.opacity = '1';
+		imageUrlInput.style.margin = '0';
+		imageUrlInput.style.padding = '0';
 
-        newColor.disabled = true;
-    } else {
-        imageUrlInput.style.maxHeight = '0';
-        imageUrlInput.style.opacity = '0';
+		newColor.disabled = true;
+	} else {
+		imageUrlInput.style.maxHeight = '0';
+		imageUrlInput.style.opacity = '0';
+		imageUrlInput.style.margin = '5px';
+		imageUrlInput.style.padding = '5px';
 
-        newColor.disabled = false;
-    }
+		newColor.disabled = false;
+	}
 });
 
 document.getElementById('save').addEventListener('click', function () {
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    status.style.opacity = '1';
-    status.style.maxHeight = '100px';
+	var status = document.getElementById('status');
+	status.textContent = 'Options saved.';
+	status.style.opacity = '1';
+	status.style.maxHeight = '100px';
 
-    setTimeout(function () {
-        status.style.opacity = '0';
-        status.style.maxHeight = '0';
-    }, 500);
+	setTimeout(function () {
+		status.style.opacity = '0';
+		status.style.maxHeight = '0';
+	}, 500);
 });
+
+document.getElementById('colorTab').addEventListener('click', function (event) {
+	openTab(event, 'Color');
+});
+
+document.getElementById('nameTab').addEventListener('click', function (event) {
+	openTab(event, 'Name');
+});
+
+function openTab(evt, tabName) {
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.opacity = '0';
+		tabcontent[i].style.maxHeight = '0';
+		tabcontent[i].style.margin = '0'; // Set margin to 0
+		tabcontent[i].style.padding = '0'; // Set padding to 0
+	}
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+	var activeTabContent = document.getElementById(tabName);
+	activeTabContent.style.display = "block";
+	setTimeout(function () {
+		activeTabContent.style.opacity = '1';
+		activeTabContent.style.maxHeight = '300px'; /* You can adjust this value based on your content size */
+		activeTabContent.style.margin = ''; // Reset margin
+		activeTabContent.style.padding = ''; // Reset padding
+	}, 500);
+	evt.currentTarget.className += " active";
+}
